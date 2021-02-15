c1906f2ac2a84dae1389a51d0064a6397419bc84
joelohrdev <55007850+joelohrdev@users.noreply.github.com>
2021-02-14 20:45:50 -0600

    Livewire and Tailwind Installed

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>Softball Stats</title>
+    @livewireStyles
+</head>
+<body>
+@livewireScripts
+</body>
+</html>
