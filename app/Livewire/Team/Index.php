5ce4cc1e132099da3887622f7f79616b9a082daa
Joe <joe@MacBookPro.attlocal.net>
2025-02-24 22:02:08 -0600

    Team index

--- app/Livewire/Team/Index.php
+++ app/Livewire/Team/Index.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Team;
 
+use App\Models\Team;
 use Illuminate\View\View;
 use Livewire\Attributes\Title;
 use Livewire\Component;
@@ -11,6 +12,8 @@ class Index extends Component
     #[Title('Teams')]
     public function render(): View
     {
-        return view('livewire.team.index');
+        return view('livewire.team.index', [
+            'teams' => Team::query()->orderBy('year')->get(),
+        ]);
     }
 }
